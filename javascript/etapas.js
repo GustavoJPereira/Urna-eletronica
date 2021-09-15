71258e45b07da8c4546dc2a48d38060a75e9d0bb
Escravo <88740554+Escravo@users.noreply.github.com>
2021-09-15 10:05:47 -0300

    imagens, pastas e html

---
+++ javascript/etapas.js
@@ -0,0 +1,57 @@
+let etapas = [{
+        titulo: 'VEREADOR',
+        numero: 5,
+        candidatos: [{
+                numero: 38111,
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [{
+                    url: '1.png',
+                    legenda: 'Vereador'
+                }]
+            },
+            {
+                numero: 77222,
+                nome: 'Ciltrano de Tal',
+                partido: 'DEFG',
+                fotos: [{
+                    url: '5.jpg',
+                    legenda: 'Vereador'
+                }]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numero: 2,
+        candidatos: [{
+                numero: 99,
+                nome: 'Fulano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [{
+                    url: '6.jpg',
+                    legenda: 'Prefeito'
+                }, {
+                    url: '1.png',
+                    legenda: 'Vice-Prefeito',
+                    small: true
+                }]
+            },
+            {
+                numero: 99,
+                nome: 'Zulano',
+                partido: 'QWERYY',
+                vice: 'Zul',
+                fotos: [{
+                    url: '4.jpg',
+                    legenda: 'Prefeito'
+                }, {
+                    url: '1.png',
+                    legenda: 'Vice-Prefeito',
+                    small: true
+                }]
+            }
+        ]
+    }
+]
